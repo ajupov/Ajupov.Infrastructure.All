e4a8c3155f9962e7df6e306f585b97acf2d31f10
Usman Ayupov <u.ayupov@dodopizza.com>
2020-01-12 13:47:06 +0300

    Add jwt default claim types

--- Jwt/JwtDefaults.cs
+++ Jwt/JwtDefaults.cs
@@ -3,5 +3,19 @@
     public static class JwtDefaults
     {
         public const string Scheme = "LiteCRM Identity";
+
+        public const string IdentifierClaimType = "id";
+
+        public const string SurnameClaimType = "surname";
+
+        public const string NameClaimType = "name";
+
+        public const string EmailClaimType = "email";
+
+        public const string PhoneClaimType = "phone";
+
+        public const string GenderClaimType = "gender";
+
+        public const string BirthDate = "birth_date";
     }
 }
