a5ba9579922b55e5150b6a3b1e073aed3e943ece
Usman Ayupov <u.ayupov@dodopizza.com>
2020-01-25 21:44:33 +0300

    Add client ip forwarding

--- Mvc/DefaultMvcController.cs
+++ Mvc/DefaultMvcController.cs
@@ -4,7 +4,7 @@ namespace Ajupov.Infrastructure.All.Mvc
 {
     public class DefaultMvcController : Controller
     {
-        protected string IpAddress => Request.HttpContext.Connection.RemoteIpAddress.ToString();
+        protected string IpAddress => Request.Headers["X-Real-IP"].ToString();
 
         protected string UserAgent => Request.Headers["User-Agent"].ToString();
     }
