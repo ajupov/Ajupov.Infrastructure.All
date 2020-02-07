98f09c9908ef35f0957d3323976a10a51c51e573
Usman Ayupov <u.ayupov@dodopizza.com>
2020-02-07 21:16:14 +0300

    Add content type attributes

---
+++ Mvc/Attributes/RequestContentTypeApplicationJsonAttribute.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ajupov.Infrastructure.All.Mvc.Attributes
+{
+    public class RequestContentTypeApplicationJsonAttribute : ConsumesAttribute
+    {
+        public RequestContentTypeApplicationJsonAttribute() : base("application/json")
+        {
+        }
+    }
+}
---
+++ Mvc/Attributes/ResponseContentTypeApplicationJsonAttribute.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ajupov.Infrastructure.All.Mvc.Attributes
+{
+    public class ResponseContentTypeApplicationJsonAttribute : ProducesAttribute
+    {
+        public ResponseContentTypeApplicationJsonAttribute() : base("application/json")
+        {
+        }
+    }
+}
