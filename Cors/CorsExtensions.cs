db96a7e2bef3da070bbccf0685423d8d2ab3894a
Usman Ayupov <u.ayupov@dodopizza.com>
2020-02-14 17:17:30 +0300

    Add CorsExtensions

---
+++ Cors/CorsExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ajupov.Infrastructure.All.Cors
+{
+    public static class CorsExtensions
+    {
+        private const string SingleOriginCorsPolicy = "SingleOriginCorsPolicy";
+
+        public static IServiceCollection AddSingleOriginCorsPolicy(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var allowedCorsOrigin = configuration["AllowedCorsOrigin"];
+            if (string.IsNullOrWhiteSpace(allowedCorsOrigin))
+            {
+                throw new Exception("AllowedCorsOrigin is empty");
+            }
+
+            return services
+                .AddCors(x => x.AddPolicy(SingleOriginCorsPolicy, builder =>
+                {
+                    builder
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                        .WithOrigins(allowedCorsOrigin);
+                }));
+        }
+
+        public static IApplicationBuilder UseSingleOriginCors(this IApplicationBuilder applicationBuilder)
+        {
+            return applicationBuilder
+                .UseCors(SingleOriginCorsPolicy);
+        }
+    }
+}
