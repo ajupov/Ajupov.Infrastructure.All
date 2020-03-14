28b8377b46e6c4d103a58cd5cfca2f519248642d
Usman Ayupov <u.ayupov@dodopizza.com>
2020-03-14 12:35:45 +0300

    Update packages

--- TestsDependencyInjection/BaseStartup.cs
+++ TestsDependencyInjection/BaseStartup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Xunit.DependencyInjection;
 using Xunit.Sdk;
 
@@ -10,11 +12,18 @@ namespace Ajupov.Infrastructure.All.TestsDependencyInjection
         {
         }
 
-        protected sealed override void ConfigureServices(IServiceCollection services)
+        protected void ConfigureServices(IServiceCollection services)
         {
             Configure(services);
 
             services.BuildServiceProvider();
+        }
+
+        protected override IHostBuilder CreateHostBuilder(AssemblyName assemblyName)
+        {
+            return base
+                .CreateHostBuilder(assemblyName)
+                .ConfigureServices(ConfigureServices);
         }
 
         protected virtual void Configure(IServiceCollection services)
