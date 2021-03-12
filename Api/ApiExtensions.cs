1f893277e1660681a37aed8dd03a068eb16226dc
Usman Ayupov <u.ayupov@dodopizza.com>
2021-03-12 15:47:27 +0300

    AddControllersWithViews

--- Api/ApiExtensions.cs
+++ Api/ApiExtensions.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Linq;
+using Ajupov.Infrastructure.All.Api.Filters;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ajupov.Infrastructure.All.Api
 {
-    public static class MvcExtensions
+    public static class ApiExtensions
     {
         public static IServiceCollection AddControllers(this IServiceCollection services, params Type[] filters)
         {
@@ -13,6 +15,23 @@ namespace Ajupov.Infrastructure.All.Api
                 .AddControllers(x =>
                 {
                     x.EnableEndpointRouting = false;
+                    x.Filters.Add(typeof(ValidationFilter));
+                    filters.ToList().ForEach(f => x.Filters.Add(f));
+                });
+
+            return services;
+        }
+
+        public static IServiceCollection AddControllersWithViews(
+            this IServiceCollection services,
+            params Type[] filters)
+        {
+            services
+                .AddControllersWithViews(x =>
+                {
+                    x.EnableEndpointRouting = false;
+                    x.Filters.Add(typeof(ValidationFilter));
+                    x.Filters.Add(typeof(AutoValidateAntiforgeryTokenAttribute));
                     filters.ToList().ForEach(f => x.Filters.Add(f));
                 });
 
